8062dda6cde3830932786f9e1a2c3758631f6c8b
Jimmy Torres <yimmytor@yahoo.es>
2021-04-30 08:13:35 -0600

    Se finaliza el modal de reserva

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 (()=>{
     function main() {
         iniciarListeners();
@@ -5,11 +7,17 @@
 
     function iniciarListeners() {
         const opcionesMenu = document.querySelectorAll('nav ul li');
+        const solicitarReserva = document.querySelector('#solicitar-reserva');
+        const aceptarModal = document.querySelector('#aceptar-modal');
+
         window.addEventListener('scroll', mostrarFondoHeader);
 
         opcionesMenu.forEach(opcion => {
             opcion.addEventListener('click', ocultarMenu);
         });
+
+        solicitarReserva.addEventListener('click', rellenarModal);
+        aceptarModal.addEventListener('click', ocultarModal);
     }   
 
     function mostrarFondoHeader() {
@@ -21,9 +29,62 @@
     }
 
     function ocultarMenu() {
-        document.querySelector('#menu-bar').checked = false;        
+        document.querySelector('#menu-bar').checked = false;                
+    }
 
-        
+    function mostrarOcultarModal() {
+        const modal = document.querySelector('#modal');
+
+        modal.classList.toggle('oculto');
+    }
+
+    function rellenarModal(event) {
+        event.preventDefault();
+
+        const modalNombre = document.querySelector('#modal-nombre');
+        const modalCorreo = document.querySelector('#modal-correo');
+        const modalTelefono = document.querySelector('#modal-telefono');
+        const modalFechaHora = document.querySelector('#modal-fecha-hora');
+        const modalComensales = document.querySelector('#modal-comensales');
+        const nombre = document.querySelector('#nombre').value;
+        const correo = document.querySelector('#correo').value;
+        const telefono = document.querySelector('#telefono').value;
+        const fecha = new Date(document.querySelector('#fecha').value);
+        const comensales = document.querySelector('#comensales').value;        
+
+        modalNombre.textContent = nombre;
+        modalCorreo.textContent = correo;
+        modalTelefono.textContent = telefono;
+        modalFechaHora.textContent = fecha.toLocaleDateString() + ' ' + fecha.toLocaleTimeString();    
+        modalComensales.textContent = comensales;
+
+        mostrarOcultarModal();
+    }
+
+    function ocultarModal() {
+        limpiarForm();
+        limpiarModal();
+        mostrarOcultarModal();
+    }
+
+    function limpiarForm() {
+        const formulario = document.querySelector('#formulario-reserva');
+
+        formulario.reset();
+    }
+
+    function limpiarModal() {
+        const modalNombre = document.querySelector('#modal-nombre');
+        const modalCorreo = document.querySelector('#modal-correo');
+        const modalTelefono = document.querySelector('#modal-telefono');
+        const modalFechaHora = document.querySelector('#modal-fecha-hora');
+        const modalComensales = document.querySelector('#modal-comensales');
+
+        modalNombre.textContent = '';
+        modalCorreo.textContent = '';
+        modalTelefono.textContent = '';
+        modalFechaHora.textContent = '';
+        modalComensales.textContent = '';
     }
 
     main();
