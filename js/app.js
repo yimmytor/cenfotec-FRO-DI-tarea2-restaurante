7f4fa8adbf19aa4e5cb65c245066453e11e0c448
Jimmy Torres <yimmytor@yahoo.es>
2021-04-30 10:48:48 -0600

    Se agrega una validacion de campos al formulario

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@
             opcion.addEventListener('click', ocultarMenu);
         });
 
-        solicitarReserva.addEventListener('click', rellenarModal);
+        solicitarReserva.addEventListener('click', validarDatosFormulario);
         aceptarModal.addEventListener('click', ocultarModal);
     }   
 
@@ -38,25 +38,18 @@
         modal.classList.toggle('oculto');
     }
 
-    function rellenarModal(event) {
-        event.preventDefault();
-
+    function rellenarModal(reserva) {
         const modalNombre = document.querySelector('#modal-nombre');
         const modalCorreo = document.querySelector('#modal-correo');
         const modalTelefono = document.querySelector('#modal-telefono');
         const modalFechaHora = document.querySelector('#modal-fecha-hora');
-        const modalComensales = document.querySelector('#modal-comensales');
-        const nombre = document.querySelector('#nombre').value;
-        const correo = document.querySelector('#correo').value;
-        const telefono = document.querySelector('#telefono').value;
-        const fecha = new Date(document.querySelector('#fecha').value);
-        const comensales = document.querySelector('#comensales').value;        
+        const modalComensales = document.querySelector('#modal-comensales');       
 
-        modalNombre.textContent = nombre;
-        modalCorreo.textContent = correo;
-        modalTelefono.textContent = telefono;
-        modalFechaHora.textContent = fecha.toLocaleDateString() + ' ' + fecha.toLocaleTimeString();    
-        modalComensales.textContent = comensales;
+        modalNombre.textContent = reserva.nombre;
+        modalCorreo.textContent = reserva.correo;
+        modalTelefono.textContent = reserva.telefono;
+        modalFechaHora.textContent = reserva.fecha.toLocaleDateString() + ' ' + reserva.fecha.toLocaleTimeString();    
+        modalComensales.textContent = reserva.comensales;
 
         mostrarOcultarModal();
     }
@@ -86,6 +79,56 @@
         modalFechaHora.textContent = '';
         modalComensales.textContent = '';
     }
+
+    function validarDatosFormulario(event) {
+        event.preventDefault();
+
+        const nombre = document.querySelector('#nombre').value;
+        const correo = document.querySelector('#correo').value;
+        const telefono = document.querySelector('#telefono').value;
+        const fecha = new Date(document.querySelector('#fecha').value);
+        const comensales = document.querySelector('#comensales').value;        
+        
+        if (nombre !== '' && correo !== '' && telefono !== '' && fecha != 'Invalid Date' && comensales !== '') {
+            if(validarEmail(correo)){
+                rellenarModal({
+                    nombre,
+                    correo,
+                    telefono,
+                    fecha,
+                    comensales
+                });
+            } else {
+                mostrarError('Formato de correo incorrecto');
+            }            
+        } else {
+            mostrarError('Debe completar todos los datos.');
+        }
+    }
+
+    function mostrarError(textoMensaje) {
+        const mensaje = document.querySelector('#contenedor-mensaje-error');
+        const parrafo = document.createElement('p');
+        const texto = document.createTextNode(textoMensaje);
+
+        parrafo.appendChild(texto);
+        parrafo.classList.add('mensaje-error');
+
+        while(mensaje.firstChild) {
+            mensaje.removeChild(mensaje.firstChild);
+        }
+
+        mensaje.appendChild(parrafo);
+
+        setInterval(() => {
+            parrafo.remove();
+        }, 3000);
+    }
+
+    function validarEmail(valor) {
+        return /^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.exec(valor);
+    }
+    
 
     main();
 })();
